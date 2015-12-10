8334471f4722113e5624695a59c1753e0bb12124
selesdepselesnul <selesdepselesnul@gmail.com>
2015-12-10 14:57:37 +0700

    init

---
+++ app/controllers/RootController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ */
+class RootController
+{
+
+    public function index()
+    {
+        echo "Index page";
+    }
+
+    public function login()
+    {
+        echo "Login page";
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require('lib/base.php');
+$f3 = Base::instance();
+$f3->config('config/config.ini');
+$f3->config('config/route.ini');
+$f3->run();
+?>
