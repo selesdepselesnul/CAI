62526cb91d278912c0d8b0d2f4ab74b2a88f2160
selesdepselesnul <selesdepselesnul@gmail.com>
2015-12-11 20:59:22 +0700

    update auth

--- app/controllers/RootController.php
+++ app/controllers/RootController.php
@@ -1,17 +1,41 @@
 <?php
 /**
- *
+ *@author : Moch Deden (https://github.com/selesdepselesnul)
  */
 class RootController
 {
+    public function __construct()
+    {
+        $this->f3 = Base::instance();
+    }
 
     public function index()
     {
-        echo "Index page";
+        
+        echo "index page";
+        // if ($this->f3->get('COOKIE["isLoggin"]') == 'no')
+        //     $this->f3->reroute('login/');
     }
 
     public function login()
     {
-        echo "Login page";
+        echo \Template::instance()->render('login.htm');
+    }
+
+    public function postLogin()
+    {
+        $db = new DB\SQL(
+            $this->f3->get('db.dns'),
+            $this->f3->get('db.user'),
+            $this->f3->get('db.password')
+        );
+        
+        $admin = new Admin($db);
+        $username = $this->f3->get('POST["username"]');
+        $password = $this->f3->get('POST["password"]');
+
+        if($admin->password == $password)
+            $this->f3->reroute('/');
+        
     }
 }
---
+++ app/models/Admin.php
@@ -0,0 +1,10 @@
+<?php
+class Admin extends DB\SQL\Mapper
+{
+	public function __construct($db) 
+	{
+		parent::__construct($db, 'admins');
+		$this->load(['username = ?', 'root']);
+	}
+
+}
